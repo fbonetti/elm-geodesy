12b067b45db348b078572d4d2827c777ce83b603
Frank Bonetti <frank.r.bonetti@gmail.com>
2016-06-05 19:52:14 -0500

    Add more tests

--- src/Geodesy.elm
+++ src/Geodesy.elm
@@ -1,6 +1,13 @@
-module Geodesy exposing (Coordinate, Unit(..), distance, decimalDegrees, midpoint, initialBearing, finalBearing, rhumbDistance)
+module Geodesy exposing (Coordinate, DegreesMinutesSeconds, Unit(..), distance, decimalDegrees, degreesMinutesSeconds, midpoint, initialBearing, finalBearing, rhumbDistance, rhumbBearing, rhumbMidpoint)
 
-type alias Coordinate = (Float, Float)
+
+type alias Coordinate =
+  ( Float, Float )
+
+
+type alias DegreesMinutesSeconds =
+  ( Int, Int, Float )
+
 
 type Unit
   = Kilometers
@@ -8,100 +15,211 @@ type Unit
   | Miles
   | Feet
 
+
 earthRadius : Unit -> Float
 earthRadius unit =
   case unit of
-    Kilometers -> 6371.0
-    Meters -> 6371000
-    Miles -> 3958.8
-    Feet -> 5280 * 3958.8
+    Kilometers ->
+      6371.0
+
+    Meters ->
+      6371000
+
+    Miles ->
+      3958.8
+
+    Feet ->
+      5280 * 3958.8
+
+
 
 -- HELPERS
+
 
 toRadians : Float -> Float
 toRadians num =
   num * pi / 180
 
+
 toDegrees : Float -> Float
 toDegrees num =
   num * 180 / pi
+
 
 floatMod : Float -> Float -> Float
 floatMod a b =
   a - (b * (toFloat << floor) (a / b))
 
-decimalDegrees : Int -> Int -> Int -> Float
-decimalDegrees degrees minutes seconds =
-  (toFloat degrees) + ((toFloat minutes) / 60) + ((toFloat seconds) / 3600)
+
+
+{- Natural log -}
+
+
+ln : Float -> Float
+ln =
+  logBase e
+
+
+isFinite : Float -> Bool
+isFinite num =
+  (not << isNaN) num && (not << isInfinite) num
+
+
+
+{- Converts degrees, minutes, and seconds into decimal degrees -}
+
+
+decimalDegrees : DegreesMinutesSeconds -> Float
+decimalDegrees ( degrees, minutes, seconds ) =
+  (toFloat degrees) + ((toFloat minutes) / 60) + (seconds / 3600)
+
+
+
+{- Converts decimal degrees into degrees, minutes, and seconds -}
+
+
+degreesMinutesSeconds : Float -> DegreesMinutesSeconds
+degreesMinutesSeconds decimal =
+  let
+    degrees =
+      floor decimal
+
+    minutes =
+      (decimal - (toFloat degrees)) * 60
+
+    minutes' =
+      floor minutes
+
+    seconds =
+      (minutes - (toFloat minutes')) * 60
+  in
+    ( degrees, minutes', seconds )
+
+
 
 -- FUNCTIONS
 
+
 distance : Coordinate -> Coordinate -> Unit -> Float
-distance (lat1,lon1) (lat2,lon2) unit =
+distance ( lat1, lon1 ) ( lat2, lon2 ) unit =
   let
-    lat1' = toRadians lat1
-    lat2' = toRadians lat2
-    lon1' = toRadians lon1
-    lon2' = toRadians lon2
+    lat1' =
+      toRadians lat1
 
-    radius = earthRadius unit
-    deltaLat = lat2' - lat1'
-    deltaLon = lon2' - lon1'
+    lat2' =
+      toRadians lat2
 
-    a = sin (deltaLat / 2) * sin (deltaLat / 2) +
-        cos lat1' * cos lat2' *
-        sin (deltaLon / 2) * sin (deltaLon / 2)
+    lon1' =
+      toRadians lon1
 
-    c = 2 * (atan2 (sqrt a) (sqrt (1 - a)))
+    lon2' =
+      toRadians lon2
+
+    radius =
+      earthRadius unit
+
+    deltaLat =
+      lat2' - lat1'
+
+    deltaLon =
+      lon2' - lon1'
+
+    a =
+      sin (deltaLat / 2)
+        * sin (deltaLat / 2)
+        + cos lat1'
+        * cos lat2'
+        * sin (deltaLon / 2)
+        * sin (deltaLon / 2)
+
+    c =
+      2 * (atan2 (sqrt a) (sqrt (1 - a)))
   in
     radius * c
 
+
 midpoint : Coordinate -> Coordinate -> Coordinate
-midpoint (lat1, lon1) (lat2, lon2) =
+midpoint ( lat1, lon1 ) ( lat2, lon2 ) =
   let
-    lat1' = toRadians lat1
-    lon1' = toRadians lon1
-    lat2' = toRadians lat2
+    lat1' =
+      toRadians lat1
 
-    deltaLon = toRadians (lon2 - lon1)
+    lon1' =
+      toRadians lon1
 
-    bx = cos lat2' * cos deltaLon
-    by = cos lat2' * sin deltaLon
+    lat2' =
+      toRadians lat2
 
-    x = sqrt (((cos lat1') + bx) * ((cos lat1') + bx) + by * by)
-    y = sin lat1' + sin lat2'
+    deltaLon =
+      toRadians (lon2 - lon1)
 
-    lat3 = atan2 y x
-    lon3 = lon1' + (atan2 by (cos lat1' + bx))
-    lon3' = floatMod ((toDegrees lon3) + 540) 360 - 180
+    bx =
+      cos lat2' * cos deltaLon
+
+    by =
+      cos lat2' * sin deltaLon
+
+    x =
+      sqrt (((cos lat1') + bx) * ((cos lat1') + bx) + by * by)
+
+    y =
+      sin lat1' + sin lat2'
+
+    lat3 =
+      atan2 y x
+
+    lon3 =
+      lon1' + (atan2 by (cos lat1' + bx))
+
+    lon3' =
+      floatMod ((toDegrees lon3) + 540) 360 - 180
   in
-    (toDegrees lat3, lon3')
+    ( toDegrees lat3, lon3' )
+
 
 initialBearing : Coordinate -> Coordinate -> Float
-initialBearing (lat1, lon1) (lat2, lon2) =
+initialBearing ( lat1, lon1 ) ( lat2, lon2 ) =
   let
-    lat1' = toRadians lat1
-    lat2' = toRadians lat2
-    deltaLon = toRadians (lon2 - lon1)
+    lat1' =
+      toRadians lat1
 
-    y = sin (deltaLon) * cos lat2'
-    x = cos lat1' * sin lat2' - sin lat1' * cos lat2' * cos deltaLon
+    lat2' =
+      toRadians lat2
+
+    deltaLon =
+      toRadians (lon2 - lon1)
+
+    y =
+      sin (deltaLon) * cos lat2'
+
+    x =
+      cos lat1' * sin lat2' - sin lat1' * cos lat2' * cos deltaLon
   in
     toDegrees (atan2 y x)
+
 
 finalBearing : Coordinate -> Coordinate -> Float
 finalBearing start destination =
   floatMod ((initialBearing destination start) + 180) 360
 
-rhumbDistance : Coordinate -> Coordinate -> Unit -> Float
-rhumbDistance (lat1, lon1) (lat2, lon2) unit =
-  let
-    radius = earthRadius unit
-    lat1' = toRadians lat1
-    lat2' = toRadians lat2
 
-    deltaLat = lat2' - lat1'
-    deltaLon = (toRadians << abs) (lon2 - lon1)
+rhumbDistance : Coordinate -> Coordinate -> Unit -> Float
+rhumbDistance ( lat1, lon1 ) ( lat2, lon2 ) unit =
+  let
+    radius =
+      earthRadius unit
+
+    lat1' =
+      toRadians lat1
+
+    lat2' =
+      toRadians lat2
+
+    deltaLat =
+      lat2' - lat1'
+
+    deltaLon =
+      (toRadians << abs) (lon2 - lon1)
 
     deltaLon' =
       if abs deltaLon > pi then
@@ -112,15 +230,97 @@ rhumbDistance (lat1, lon1) (lat2, lon2) unit =
       else
         deltaLon
 
-    projectedLatDelta = logBase e ((tan (lat2' / 2 + pi / 4)) / tan (lat1' / 2 + pi / 4))
+    projectedLatDelta =
+      ln ((tan (lat2' / 2 + pi / 4)) / tan (lat1' / 2 + pi / 4))
 
     q =
-      if abs projectedLatDelta > 10e-12 then
+      if abs projectedLatDelta > 1.0e-11 then
         deltaLat / projectedLatDelta
       else
         cos lat1'
 
-    a = sqrt (deltaLat * deltaLat + q * q * deltaLon' * deltaLon')
-
+    a =
+      sqrt (deltaLat * deltaLat + q * q * deltaLon' * deltaLon')
   in
     a * radius
+
+
+rhumbBearing : Coordinate -> Coordinate -> Float
+rhumbBearing ( lat1, lon1 ) ( lat2, lon2 ) =
+  let
+    lat1' =
+      toRadians lat1
+
+    lat2' =
+      toRadians lat2
+
+    deltaLon =
+      toRadians (lon2 - lon1)
+
+    deltaLon' =
+      if abs deltaLon > pi then
+        if deltaLon > 0 then
+          negate (2 * pi - deltaLon)
+        else
+          2 * pi + deltaLon
+      else
+        deltaLon
+
+    deltaPsi =
+      ln ((tan (lat2' / 2 + pi / 4)) / (tan (lat1' / 2 + pi / 4)))
+
+    theta =
+      atan2 deltaLon' deltaPsi
+  in
+    ((toDegrees theta) + 360) `floatMod` 360
+
+
+rhumbMidpoint : Coordinate -> Coordinate -> Coordinate
+rhumbMidpoint ( lat1, lon1 ) ( lat2, lon2 ) =
+  let
+    lat1' =
+      toRadians lat1
+
+    lat2' =
+      toRadians lat2
+
+    lon1' =
+      toRadians lon1
+
+    lon2' =
+      toRadians lon2
+
+    lon1'' =
+      if abs (lon2' - lon1') > pi then
+        lon1' + 2 * pi
+      else
+        lon1'
+
+    lat3 =
+      (lat1' + lat2') / 2
+
+    f1 =
+      tan (pi / 4 + lat1' / 2)
+
+    f2 =
+      tan (pi / 4 + lat2' / 2)
+
+    f3 =
+      tan (pi / 4 + lat3 / 2)
+
+    lon3 =
+      ((lon2' - lon1'') * ln f3 + lon1'' * ln f2 - lon2' * ln f1) / (ln (f2 / f1))
+
+    lon3' =
+      if (not << isFinite) lon3 then
+        (lon1'' + lon2') / 2
+      else
+        lon3
+
+    lat3Degrees =
+      toDegrees lat3
+
+    lon3Degrees =
+      (floatMod ((toDegrees lon3') + 540) 360) - 180
+  in
+    ( lat3Degrees, lon3Degrees )
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,43 +1,147 @@
-module Main exposing (main)
+module Tests exposing (main)
 
 import ElmTest exposing (..)
 import Geodesy exposing (..)
 
+
+-- Chicago to NYC
+
+
 chicagoToNycTests : List Test
 chicagoToNycTests =
-  [ test "Distance from Chicago to NYC" (assertEqual 1144 (round distanceChicagoToNyc))
-  , test "Midpoint from Chicago to NYC" (assertEqual (41.49677823440974,-80.75671256248268) (midpoint chicago newYork))
-  , test "Initial bearing from Chicago to NYC" (assertEqual 92 (round (initialBearing chicago newYork)))
-  , test "Final bearing from Chicago to NYC" (assertEqual 101 (round (finalBearing chicago newYork)))
-  , test "Rhumb line distance from Chicago to NYC" (assertEqual 1145 (round rhumbDistanceChicagoToNyc))
+  [ test "Distance from Chicago to NYC" (assertEqual 1144.2995483233492 distanceChicagoToNyc)
+  , test "Midpoint from Chicago to NYC" (assertEqual ( 41.49677823440974, -80.75671256248268 ) midpointFromChicagoToNyc)
+  , test "Initial bearing from Chicago to NYC" (assertEqual 91.95668081107792 initialBearingFromChicagoToNyc)
+  , test "Final bearing from Chicago to NYC" (assertEqual 100.97208853162614 finalBearingFromChicagoToNyc)
+  , test "Rhumb line distance from Chicago to NYC" (assertEqual 1145.4782975112635 rhumbDistanceChicagoToNyc)
+  , test "Rhumb midpoint from Chicago to NYC" (assertEqual ( 41.295449999999995, -80.78742529339144 ) rhumbMidpointChicagoToNyc)
+  , test "Rhumb bearing from Chicago to NYC" (assertEqual 96.49514816731596 rhumbBearingChicagoToNyc)
   ]
+
+
+chicago : Coordinate
+chicago =
+  ( 41.8781, -87.6298 )
+
+
+newYork : Coordinate
+newYork =
+  ( 40.7128, -74.0059 )
+
 
 distanceChicagoToNyc : Float
 distanceChicagoToNyc =
   distance chicago newYork Kilometers
 
+
+midpointFromChicagoToNyc : Coordinate
+midpointFromChicagoToNyc =
+  midpoint chicago newYork
+
+
+initialBearingFromChicagoToNyc : Float
+initialBearingFromChicagoToNyc =
+  initialBearing chicago newYork
+
+
+finalBearingFromChicagoToNyc : Float
+finalBearingFromChicagoToNyc =
+  finalBearing chicago newYork
+
+
 rhumbDistanceChicagoToNyc : Float
 rhumbDistanceChicagoToNyc =
   rhumbDistance chicago newYork Kilometers
 
-chicago : Coordinate
-chicago =
-  (41.8781, -87.6298)
 
-newYork : Coordinate
-newYork =
-  (40.7128, -74.0059)
+rhumbBearingChicagoToNyc : Float
+rhumbBearingChicagoToNyc =
+  rhumbBearing chicago newYork
+
+
+rhumbMidpointChicagoToNyc : Coordinate
+rhumbMidpointChicagoToNyc =
+  rhumbMidpoint chicago newYork
+
+
+
+-- London to Hong Kong
+
+
+londonToHongKongTests : List Test
+londonToHongKongTests =
+  [ test "Distance from London to Hong Kong" (assertEqual 9597.482581330738 distanceLondonToHongKong)
+  , test "Midpoint from London to Hong Kong" (assertEqual ( 52.90244252102981, 73.85262787435465 ) midpointFromLondonToHongKong)
+  , test "Initial bearing from London to Hong Kong" (assertEqual 57.83403659833477 initialBearingFromLondonToHongKong)
+  , test "Final bearing from London to Hong Kong" (assertEqual 145.25926589781778 finalBearingFromLondonToHongKong)
+  , test "Rhumb line distance from London to Hong Kong" (assertEqual 10407.501859178768 rhumbDistanceLondonToHongKong)
+  , test "Rhumb midpoint from London to Hong Kong" (assertEqual ( 36.9519, 62.68985625326832 ) rhumbMidpointLondonToHongKong)
+  , test "Rhumb bearing from London to Hong Kong" (assertEqual 108.12102559904315 rhumbBearingLondonToHongKong)
+  ]
+
+
+london : Coordinate
+london =
+  ( 51.5074, 0.1278 )
+
+
+hongKong : Coordinate
+hongKong =
+  ( 22.3964, 114.1095 )
+
+
+distanceLondonToHongKong : Float
+distanceLondonToHongKong =
+  distance london hongKong Kilometers
+
+
+midpointFromLondonToHongKong : Coordinate
+midpointFromLondonToHongKong =
+  midpoint london hongKong
+
+
+initialBearingFromLondonToHongKong : Float
+initialBearingFromLondonToHongKong =
+  initialBearing london hongKong
+
+
+finalBearingFromLondonToHongKong : Float
+finalBearingFromLondonToHongKong =
+  finalBearing london hongKong
+
+
+rhumbDistanceLondonToHongKong : Float
+rhumbDistanceLondonToHongKong =
+  rhumbDistance london hongKong Kilometers
+
+
+rhumbBearingLondonToHongKong : Float
+rhumbBearingLondonToHongKong =
+  rhumbBearing london hongKong
+
+
+rhumbMidpointLondonToHongKong : Coordinate
+rhumbMidpointLondonToHongKong =
+  rhumbMidpoint london hongKong
+
+
+
+-- Suite
+
 
 allTests : List Test
 allTests =
-  List.foldl
-    List.append
+  List.foldl List.append
     []
-    [ chicagoToNycTests ]
+    [ chicagoToNycTests
+    , londonToHongKongTests
+    ]
+
 
 consoleTests : Test
 consoleTests =
   suite "All Tests" allTests
+
 
 main : Program Never
 main =
